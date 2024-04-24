a7b0875f7bfb335345eb82bd1995e138bf1a0f09
Thomas Sauvage <thomas.sauvage@polytechnique.edu>
2024-04-24 15:53:27 +0200

    Formatting

--- ideal_cipher/public_key.py
+++ ideal_cipher/public_key.py
@@ -19,10 +19,13 @@ def encrypt(public_key: bytes, symmetric_key: bytes) -> bitarray:
     seed_part = bitarray()
     seed_part.frombytes(public_key[-SEED_BYTES_LENGTH:])
     encoded_value_part = encoder.encode(value_part)
+
     value_being_encrypted = encoded_value_part
+
     encrypted_value_part = None
     for _ in range(NB_FEISTEL_ITERATIONS):
         value_being_encrypted = feistel.encrypt(symmetric_key, value_being_encrypted)
+
         if (
             sum(
                 value_being_encrypted[-i - 1] << i
@@ -33,10 +36,12 @@ def encrypt(public_key: bytes, symmetric_key: bytes) -> bitarray:
             and encrypted_value_part is None
         ):
             encrypted_value_part = value_being_encrypted
+
     if encrypted_value_part is None:
         raise ValueError(
             f"Encryption could not be completed in {NB_FEISTEL_ITERATIONS} Feistel iterations."
         )
+
     return encrypted_value_part + feistel.encrypt(symmetric_key, seed_part)
 
 
@@ -44,10 +49,13 @@ def decrypt(encrypted_public_key: bitarray, symmetric_key: bytes) -> bytes:
     seed_part = feistel.decrypt(
         symmetric_key, encrypted_public_key[-SEED_BYTES_LENGTH * 8 :]
     ).tobytes()
+
     value_being_decrypted = encrypted_public_key[: -SEED_BYTES_LENGTH * 8]
+
     decrypted_value_part = None
     for _ in range(NB_FEISTEL_ITERATIONS):
         value_being_decrypted = feistel.decrypt(symmetric_key, value_being_decrypted)
+
         if (
             sum(
                 value_being_decrypted[-i - 1] << i
@@ -57,8 +65,10 @@ def decrypt(encrypted_public_key: bitarray, symmetric_key: bytes) -> bytes:
             and decrypted_value_part is None
         ):
             decrypted_value_part = value_being_decrypted
+
     if decrypted_value_part is None:
         raise ValueError(
             f"Decryption could not be completed in {NB_FEISTEL_ITERATIONS} Feistel iterations."
         )
+
     return encoder.decode(decrypted_value_part) + seed_part
